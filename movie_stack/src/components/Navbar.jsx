9fecbcda2f82c83e8ea563ae26ef3f5999a37928
Emmanuel Oginga <ogolemmanuel3@gmail.com>
2025-08-21 21:25:43 +0300

    style navbar componet for large screens

--- movie_stack/src/components/Navbar.jsx
+++ movie_stack/src/components/Navbar.jsx
@@ -7,28 +7,33 @@ import avatar from"../assets/image-avatar.png"
 
 const Navbar = () => {
  return (
-        <nav className="flex justify-between  bg-blue-950 px-[17px] py-[17px] items-center">
-    <div className="navbar__logo">
-      <img src={logoIcon} alt="Logo" className="w-[32px] h-[25.6px]"/>
+    <div className="bg-blue-900 md:pt-6">
+       <nav className="flex justify-between  bg-blue-950 px-[17px] py-[17px] items-center md:w-[720px] 
+          md:h-18 md:px-[21px]  md:mx-auto md:rounded-xl md:py-[21px] lg:flex-col
+           lg:ml-8 lg:w-24 lg:h-240 lg:px-7 lg:py-8 lg:rounded-[20px] ">
+    <div className="">
+      <img src={logoIcon} alt="Logo" className="w-[32px] h-[25.6px] "/>
     </div>
-    <ul className="flex items-center  gap-[24px]">
+    <ul className="flex items-center lg:flex-col  gap-[24px] lg:mt-[6px]   lg:h-[200px]  lg:justify-between lg:mt-[97.6px]">
       <li>
-        <img src={homeIcon} alt="Home" className="w-[16px] h-[16px]"/>
+        <img src={homeIcon} alt="Home" className="w-[16px] h-[16px] md:w-5 md:h-5"/>
       </li>
       <li>
-        <img src={moviesIcon} alt="Movies" className="w-[16px] h-[16px]"/>
+        <img src={moviesIcon} alt="Movies" className="w-4 h-4 md:w-5 md:h-5"/>
       </li>
       <li>    
-        <img src={tvIcon} alt="TV Series" className="w-[16px] h-[16px]" />
+        <img src={tvIcon} alt="TV Series" className="w-4 h-4 md:w-5 md:h-5" />
       </li>
       <li>
-        <img src={bookmarkIcon} alt="Bookmarks" className="w-[14px]"/>
+        <img src={bookmarkIcon} alt="Bookmarks" className="w-[14px] md:w-[16.96px] md:h-5"/>
       </li>
     </ul>
-    <div className="">
-      <img src={avatar} alt="User Avatar" className="w-[24px] h-[24px] "/>
+    <div className="lg:mt-[515px]">
+      <img src={avatar} alt="User Avatar" className="w-6 h-6 md:h-8 md:w-8 lg:w-[40px] lg:h-[40px]"/>
     </div>
   </nav>
+
+    </div>
 
  );
 }
