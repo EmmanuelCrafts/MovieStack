685e6b7384dda9bcb99ad10dfa44779055334405
Emmanuel Oginga <ogolemmanuel3@gmail.com>
2025-09-03 22:08:37 +0300

    feat: Create a tvSeries component  and import Navbar, SearchBar and PopularSeries components and display them

---
+++ movie_stack/src/pages/TvSeries.jsx
@@ -0,0 +1,19 @@
+import Navbar from '../components/Navbar'
+import SearchBar from '../components/SearchBar'
+import PopularSeries from '../components/PopularSeries'
+
+const HomePage = () => {
+  return (
+      <div className=" min-h-screen grid lg:grid-cols-[96px_1fr] grid-cols-1 gap-y-4 lg:gap-x-8 lg:py-4"> 
+         <aside className='lg:sticky lg:top-0 lg:pb-4 lg:max-h-min '>
+            <Navbar />
+         </aside>
+         <main className='overflow-x-auto  flex flex-col pb-4'>
+            <SearchBar />
+            <PopularSeries />
+         </main>
+      </div>
+     
+  )
+}
+export default HomePage;
